c3ac83f9d362711ca2634402f6e42fd1fd20301c
Matthew McEachen <matthew+github@mceachen.org>
2013-07-14 13:47:41 -0700

    scale_factor35efl is ignorable for < 9

--- test/exiftoolr_test.rb
+++ test/exiftoolr_test.rb
@@ -66,7 +66,7 @@ describe Exiftoolr do
   def ignorable_keys
     @ignorable_keys ||= begin
       ignorable = [:file_permissions, :file_access_date, :file_modify_date, :directory, :source_file, :exif_tool_version]
-      ignorable += [:modify_date, :create_date, :date_time_original, :nd_filter, :scale_factor35efl] if Exiftoolr.exiftool_version <= 8.2
+      ignorable += [:modify_date, :create_date, :date_time_original, :nd_filter, :scale_factor35efl] if Exiftoolr.exiftool_version < 9
       ignorable
     end
   end
